c8b66241250d7305a0e19ee981c1fc16ff4d4bc7
Gary <Gary.Dix@enform.nz>
2022-12-01 15:34:25 +1300

    Claims manager made

---
+++ Pages/ClaimsManager/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace RolesForAssessment.Pages.ClaimsManager
+{
+    public class IndexModel : PageModel
+    {
+        //import the userManger and generate a list of users
+        public UserManager<IdentityUser> UserManager { get; set; }
+        public IndexModel(UserManager<IdentityUser> userManager)
+        {
+            UserManager = userManager;
+        }
+        public List<IdentityUser> Users { get; set; }
+        public async Task OnGetAsync()
+        {
+            Users = await UserManager.Users.ToListAsync();
+        }
+
+    }
+}
