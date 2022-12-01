69461effc6e85278c3c1e3c20934aa519e8eac0a
Gary <Gary.Dix@enform.nz>
2022-12-01 13:30:10 +1300

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;   //add this
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace RolesForAssessment.Pages
+{
+    [Authorize] //add this
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
